d0a0b7ff67befeada2ce848591286f44971abe75
heathiscode <heathiscode@gmail.com>
2014-11-23 09:24:57 -0500

    Update dragon-dropon.js
    Fixes from removing jQuery dependencies

--- dragon-dropon.js
+++ dragon-dropon.js
@@ -7,49 +7,62 @@
     }).directive('dragOn',['ddDragState', '$parse', function (ddDragState, $parse) {
         return {
             scope: {
-              ngModel: '=',
-	      type: '@',
-              ghost: '@',
-              dragOffset: '@'
+                ngModel: '=',
+    	        type: '@',
+                ghost: '@',
+                dragOffset: '@'
             },
             link: function (scope,elem,attrs) {
-                var ghost = this.ghost || true;
+                var viewportElement = document.body.parentNode;
+                var ghost = scope.ghost || true;
 
                 var offset = scope.dragOffset || { left: -20, top: -10 };
 
-                var clone = angular.element($(elem)[0].outerHTML);
+                var clone;
 
                 elem.on('mousedown', function(event) {
-                    ddDragState.dragging = elem;
-                    ddDragState.dragData = scope.ngModel || null;
-                    angular.element(elem).css('position','absolute');
+                    if (!clone) 
+                        clone = angular.element(elem[0].outerHTML);
+                    
                     if (ghost) {
                         clone.attr('id','dragOnDragGhost');
-                        clone.css('opacity',0.5);
+                        clone.css('opacity', attrs.dragOnGhostOpacity || 0.5);
                         //insert the ghosted copy of the element
-                        clone.insertAfter(elem);
+                        elem[0].parentNode.insertBefore(clone[0],elem[0]);
                     }
+                    
+                    ddDragState.dragging = elem;
+                    ddDragState.dragData = scope.ngModel || null;
+                    
+                    //attribute is set -.-
+                    angular.element(elem).css('position','absolute');
+                    angular.element(elem).css('display','block');
+                    angular.element(document.body).css('user-select','none')
+                    
                 });
 
-                angular.element('html').on('mousemove', function(event) {
+                angular.element(viewportElement).on('mousemove', function(event) {
+                    //event fires -.- 
                     if (ddDragState.dragging !== false) {
-                        angular.element(elem).css('z-index',9999);
+                        //--fires correctly-- console.log('offset,mousemove',event.pageX,event.pageY);
+                        angular.element(elem).css('zIndex',9999);
+                        elem[0].style.left = event.pageX + offset.left + 'px';
+                        
                         angular.element(elem).css('left', event.pageX + offset.left + 'px');
                         angular.element(elem).css('top',event.pageY + offset.top + 'px');
+                        
+                        event.preventDefault();
                     }
                 });
 
-                angular.element('html').on('mouseup', function(event) {
-
-                    if (ddDragState.dragging!==false) {
+                angular.element(viewportElement).on('mouseup', function(event) {
                         angular.element(elem).css('left', '');
                         angular.element(elem).css('top', '');
                         angular.element(elem).css('position', '');
                         angular.element(elem).css('z-index', '');
                         if (ghost) {
-                            angular.element('#dragOnDragGhost').remove();
+                            angular.element(document.getElementById('dragOnDragGhost')).remove();
                         }
-                    }
 
                 });
 
@@ -62,46 +75,59 @@
                 allowedTypes: '='
             },
             link: function(scope,elem,attrs) {
+                var viewportElement = document.body.parentNode;
 
-		var getDimensions = 
-	    		function(element) {
-				element = $(element);
-				var display = $(element).getStyle('display');
-				if (display != 'none' && display != null) // Safari bug
+        		var getDimensions = 
+        	    	function(element) {
+        	    	    element = angular.element(element)[0];//maybe some other element type, do this
+                        var elm = angular.element(element);
+        				var display = elm.css('display');
+            			if (display != 'none' && display != null) // Safari bug
+            				return { width: element.offsetWidth, height: element.offsetHeight};
+        
+        				// All *Width and *Height properties give 0 on elements with display none,
+        				// so enable the element temporarily
+        				var els = element.style;
+        				var originalVisibility = els.visibility;
+        				var originalPosition = els.position;
+        				var originalDisplay = els.display;
+        				els.visibility = 'hidden';
+        				els.position = 'absolute';
+        				els.display = 'block';
+        				var originalWidth = element.clientWidth;
+        				var originalHeight = element.clientHeight;
+        				els.display = originalDisplay;
+        				els.position = originalPosition;
+        				els.visibility = originalVisibility;
+        				return {width: originalWidth, height: originalHeight};
+        			};
 
-				return { width: element.offsetWidth, height: element.offsetHeight};
-
-				// All *Width and *Height properties give 0 on elements with display none,
-				// so enable the element temporarily
-				var els = element.style;
-				var originalVisibility = els.visibility;
-				var originalPosition = els.position;
-				var originalDisplay = els.display;
-				els.visibility = 'hidden';
-				els.position = 'absolute';
-				els.display = 'block';
-				var originalWidth = element.clientWidth;
-				var originalHeight = element.clientHeight;
-				els.display = originalDisplay;
-				els.position = originalPosition;
-				els.visibility = originalVisibility;
-				return {width: originalWidth, height: originalHeight};
-			};
-
-                var dim = getDimensions(element);
-                var offset = elem.offset();
+                var dim = getDimensions(elem);
+                var offset = elem[0].getBoundingClientRect();
 
                 var rect = {
                     top: offset.top, bottom: offset.top+dim.height,
                     left: offset.left, right: offset.left+dim.width
                 };
 
+                //pesky target moves
+                angular.element(viewportElement).on('mousedown', function(event) {
+                    
+                    offset = elem[0].getBoundingClientRect();
+                    rect = {
+                        top: offset.top, bottom: offset.top+dim.height,
+                        left: offset.left, right: offset.left+dim.width
+                    };
+                    
+                });
 
-                angular.element('html').on('mousemove', function(event) {
+
+                angular.element(viewportElement).on('mousemove', function(event) {
+                   
                     if (ddDragState.dragging !== false) {
                         var x = event.pageX;
                         var y = event.pageY;
-
+                
                         if (x>=rect.left && x<=rect.right && y>=rect.top && y<=rect.bottom) {
                             elem.addClass('dropon-hover');
                             ddDragState.dropTarget = elem;
@@ -118,7 +144,7 @@
                     }
                 });
 
-                angular.element('html').on('mouseup', function(event) {
+                angular.element(viewportElement).on('mouseup', function(event) {
                     elem.removeClass('dropon-hover');
 
                     if (ddDragState.dropTarget) {
@@ -134,6 +160,3 @@
             }
         }
     }]);
-
-
-
